062aff97adf1401cef48b85e16954a6c57058829
samj11 <samj11@gmail.com>
2020-10-08 22:30:28 -0400

    Implemented Wave System
    When one enemy dies, wait 1s and add another enemy on the next wave.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+
     [SerializeField]
     private GameObject _enemyPrefab;
     [SerializeField]
     private GameObject[] powerups;
+
+    private bool _enemiesAlive = false;
+    private int _waveCount = 1;
 
     [SerializeField]
     private GameObject _enemyContainer;
@@ -18,19 +22,38 @@ public class SpawnManager : MonoBehaviour
 
     public void StartSpawning()
     {
-        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnWaves());
         StartCoroutine(SpawnPowerupRoutine());
     }
 
-    IEnumerator SpawnEnemyRoutine()
+
+    IEnumerator SpawnWaves()
     {
-        while(_playerIsDead == false)
+        while (_playerIsDead == false)
         {
-            Vector3 posSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
-            GameObject newEnemy = Instantiate(_enemyPrefab, posSpawn, Quaternion.identity);
-            newEnemy.transform.parent = _enemyContainer.transform;
-            yield return new WaitForSeconds(2.0f);
+            if (_enemiesAlive == false)
+            {
+                for (int i = 0; i < _waveCount; i++)
+                {
+                    SpawnEnemy();
+                }
+                _waveCount++;
+            }
+
+            if (_enemyContainer.transform.childCount == 0)
+                _enemiesAlive = false;
+
+            yield return new WaitForSeconds(1.0f);
+            Debug.Log("number of waves" + _waveCount);
         }
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector3 posSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+        GameObject newEnemy = Instantiate(_enemyPrefab, posSpawn, Quaternion.identity);
+        newEnemy.transform.parent = _enemyContainer.transform;
+        _enemiesAlive = true;   
     }
 
     IEnumerator SpawnPowerupRoutine()
